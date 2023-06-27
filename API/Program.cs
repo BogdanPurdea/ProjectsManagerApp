9755af6c818f0d372a7bef3d06b9235cb2ebb7d1
Purdea Bogdan <bogdanpurdea14@gmail.com>
2023-06-27 20:01:52 +0300

    Added DB support with dependency injection

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,14 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
